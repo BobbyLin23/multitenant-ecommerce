7c62d6bd566ddd4ce1729499b4b7d3f1b412c458
BobbyLin <linzhangsheng23@gmail.com>
2025-04-16 20:26:26 +0800

    chore: add some custom ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Input } from '@/components/ui/input'
+import { Progress } from '@/components/ui/progress'
+import { Textarea } from '@/components/ui/textarea'
 
 export default function Home() {
   return (
-    <div className="p-2">
-      Hello World
-      <Button>Click</Button>
+    <div className="flex flex-col gap-y-2 p-2">
+      <div>
+        <Button variant="elevated">I am an Elevated Button</Button>
+      </div>
+      <div>
+        <Input />
+      </div>
+      <div>
+        <Textarea />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   )
 }
